a110b995f19184ae2a5bc9692f920febd31fbd5f
jtowne01 <151704673+jtowne01@users.noreply.github.com>
2024-09-19 21:25:52 -0400

    Update fb0.js

--- scripts/fb0.js
+++ scripts/fb0.js
@@ -12,7 +12,7 @@ document.getElementById("name-form").addEventListener("submit", function(event) 
     }
 
 
-    let greetingMessage = `Hello, ${firstName} ${middleInitial ? middleInitial + "." : ""} ${lastName}!`;
+    let greetingMessage = `Welcome to Jumpy Tarantula, ${firstName} ${middleInitial ? middleInitial + "." : ""} ${lastName}!`;
     document.getElementById("greeting").innerText = greetingMessage;
 
 
