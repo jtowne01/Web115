4f70682b8861100173d7721844f5a8827f6b9f19
jtowne01 <151704673+jtowne01@users.noreply.github.com>
2024-09-20 19:48:05 -0400

    Update fizzbuzz1.js

--- scripts/fizzbuzz1.js
+++ scripts/fizzbuzz1.js
@@ -1,20 +1,3 @@
-document.getElementById("name-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    let firstName = document.getElementById("first-name").value.trim();
-    let middleInitial = document.getElementById("middle-initial").value.trim();
-    let lastName = document.getElementById("last-name").value.trim();
- 
-    if (!firstName || !lastName) {
-        alert("First Name and Last Name are required!");
-        return;
-    }
-    let greetingMessage = `Welcome to Jumpy Tarantula, ${firstName} ${middleInitial ? middleInitial + "." : ""} ${lastName}!`;
-    document.getElementById("greeting").innerText = greetingMessage;
-
-    let countTo = 140; 
-    generateList(countTo);
-});
-
 function generateList(count) {
     const list = document.getElementById("fizz-buzz-list");
     list.innerHTML = ""; 
@@ -31,3 +14,20 @@ function generateList(count) {
         list.appendChild(listItem);
     }
 }
+
+document.getElementById("name-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    let firstName = document.getElementById("first-name").value.trim();
+    let middleInitial = document.getElementById("middle-initial").value.trim();
+    let lastName = document.getElementById("last-name").value.trim();
+ 
+    if (!firstName || !lastName) {
+        alert("First Name and Last Name are required!");
+        return;
+    }
+    let greetingMessage = `Welcome to Jumpy Tarantula, ${firstName} ${middleInitial ? middleInitial + "." : ""} ${lastName}!`;
+    document.getElementById("greeting").innerText = greetingMessage;
+
+    let countTo = 140;
+    generateList(countTo); 
+});
